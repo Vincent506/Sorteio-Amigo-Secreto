ad25ae7a7eb8e6c7a6315f634b5b07dbdbeb2571
Vincent506 <vicenteneto0806@gmail.com>
2025-11-14 11:17:19 -0300

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 function adicionar(){
     let nomeDigitado = document.getElementById('nome-amigo').value;
     if (listaDeNomes.includes(nomeDigitado)) {
-        alert('Não é permitido nomes repitidos')
-    }else{
+        alert('Não é permitido nomes repitidos');
+    }
+    else{
         listaDeNomes.push(nomeDigitado);
-       
     }
 
     let lista = document.getElementById("lista-amigos");
@@ -14,7 +14,25 @@ function adicionar(){
 }
 
 function sortear(){
-    // falta desenvolver essa parte
+    embaralhar(listaDeNomes);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    if (listaDeNomes.length < 3) {
+        alert('adicione pelo menos 3 pessoas para adicionar o sorteior');
+    }else{
+        for(let i = 0; i < listaDeNomes.length; i++ ){
+
+        if (i == listaDeNomes.length - 1) {
+            
+            sorteio.innerHTML = sorteio.innerHTML + listaDeNomes[i] + '--->' + listaDeNomes[0] + '<br>';
+
+        }else{
+            
+            sorteio.innerHTML = sorteio.innerHTML + listaDeNomes[i] + '--->' + listaDeNomes[i+1] + '<br>';
+
+        }
+     }
+    }
 }
 
 function reiniciar(){
@@ -22,5 +40,15 @@ function reiniciar(){
     listaDeNomes.splice(0,listaDeNomes.length);
     document.getElementById("lista-amigos").textContent = listaDeNomes;
     document.getElementById('lista-sorteio').textContent = listaDeNomes;
+
+}
+
+function embaralhar(lista){
+    
+    for(let indice = lista.length; indice; indice--){
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        [lista[indice-1],lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice-1]];
+    }
 
 }
